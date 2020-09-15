523a7634e11e0a61e6ba1c5971ffb97c2aad0182
sabaodecoco <cavalcanteferreiradesouza@gmail.com>
2020-09-15 15:54:24 -0300

    WIP: adicoes ao dfa

--- mgol/src/main/scala/DFA.scala
+++ mgol/src/main/scala/DFA.scala
@@ -6,16 +6,18 @@ class DFA {
 
   private val automataTransitionTable: Map[Int, Map[String, Int]] = Map(
       0 -> Map[String, Int](
+        " " -> 0, "\n" -> 0, "\t" -> 0,
         "(" -> 1,
         ")" -> 2,
         ";" -> 3,
         "letra" -> 4, //qualquer letra
         "digito" -> 5, // qualquer dígito
-        "{" -> 6,
-        "}" -> -1,  //erro
+        "{" -> 10,
         "\"" -> 12,
         "+" -> 14, "-" -> 14, "/" -> 14, "*" -> 14,
-        "logico" -> 15,
+        ">" -> 16,
+        "<" -> 15,
+        "=" -> 17,
         "-" -> 20,
         "eof" -> 19
     ),
@@ -24,7 +26,7 @@ class DFA {
     2 -> Map[String, Int](), //estado sem transições; aceitação
     3 -> Map[String, Int](), //estado sem transições; aceitação
 
-    4 -> Map[String, Int](
+    4 -> Map[String, Int]( //estado de aceitação
       "letra" -> 4, "digito" -> 4, "_" -> 4
     ),
 
@@ -41,24 +43,51 @@ class DFA {
       "+" -> 81, "-" -> 81
     ),
 
+    8 -> Map[String, Int](), //estado sem transições; aceitação
+
     81 -> Map[String, Int](
       "digito" -> 9
     ),
 
-    9 -> Map[String, Int](
+    9 -> Map[String, Int]( //estado de aceitação
       "digito" -> 9
     ),
 
     10 -> Map[String, Int](
       "digito" -> 10, "letra" -> 10, "logico" -> 10, "." -> 10, ";" -> 10, "*" -> 10, "+" -> 10,
-      "-" -> 10, "/" -> 10, "_" -> 10
+      "-" -> 10, "/" -> 10, "_" -> 10,
+      "}" -> 11
     ),
     11 -> Map[String, Int](), //estado sem transições; aceitação
 
     12 -> Map[String, Int](
       "digito" -> 12, "letra" -> 12, "logico" -> 12, "." -> 12, ";" -> 12, "*" -> 12, "+" -> 12,
-      "-" -> 12, "/" -> 12, "_" -> 12
-    )
+      "-" -> 12, "/" -> 12, "_" -> 12,
+      "" -> 13
+    ),
+
+    13 -> Map[String, Int](),//estado sem transições; aceitação
+
+    14 -> Map[String, Int](), //estado sem transições; aceitação
+
+    15 -> Map[String, Int]( //estado de aceitação
+      "=" -> 17,
+      ">" -> 17
+    ),
+
+    16 -> Map[String, Int]( //estado de aceitação
+      "=" -> 17
+    ),
+
+    17 -> Map[String, Int](), //estado sem transições; aceitação
+
+    20 -> Map[String, Int](
+      "<" -> 21
+    ),
+
+    21 -> Map[String, Int](),
+
+    22 -> Map[String, Int]() //estado sem transições; aceitação
 
   )
 
